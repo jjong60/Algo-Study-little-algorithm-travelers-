20af59ba565071055c4d61d4750b180e7aeb0441
jjong <87465224+jjong60@users.noreply.github.com>
2023-10-19 20:43:37 +0900

    [Bronze V] Title: A+B - 2, Time: 76 ms, Memory: 5464 KB -BaekjoonHub

---
+++ 백준/Bronze/2558. A＋B － 2/A＋B － 2.cs
@@ -0,0 +1,11 @@
+using System;
+
+class Algorithm
+{
+    static void Main()
+    {
+        int A = int.Parse(Console.ReadLine());
+        int B = int.Parse(Console.ReadLine());
+        Console.WriteLine(A+B);
+    }
+}
