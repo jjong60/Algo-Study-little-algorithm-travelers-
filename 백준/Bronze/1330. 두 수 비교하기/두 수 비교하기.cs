ac80625af112bfb3f912f09e7b6c227499cd6c14
jjong <87465224+jjong60@users.noreply.github.com>
2023-10-19 19:07:04 +0900

    [Bronze V] Title: 두 수 비교하기, Time: 64 ms, Memory: 5484 KB -BaekjoonHub

---
+++ 백준/Bronze/1330. 두 수 비교하기/두 수 비교하기.cs
@@ -0,0 +1,26 @@
+using System;
+using System.Numerics;
+
+class SumAlgorithm
+{
+    static void Main()
+    {
+        string[] input = Console.ReadLine().Split();
+
+        int A = int.Parse(input[0]);
+        int B = int.Parse(input[1]);
+
+        if (A > B)
+        {
+            Console.WriteLine(">");
+        }
+        else if (A < B) 
+        {
+            Console.WriteLine("<");
+        }
+        else if (A == B) 
+        {
+            Console.WriteLine("==");
+        }
+    }
+}
